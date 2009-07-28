bbedee1e8639a12e21d029f07ad671c682882b87
mr_gimo <g.paulaitis@gmail.com>
2009-07-28 16:43:13 +0200

    full screen ect.

--- src/ch/hsr/ifs/liquids/Liquids.java
+++ src/ch/hsr/ifs/liquids/Liquids.java
@@ -1,57 +1,96 @@
 package ch.hsr.ifs.liquids;
 
-import java.awt.Cursor;
 import java.awt.Frame;
+import java.awt.GraphicsDevice;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLEventListener;
 
-import ch.hsr.ifs.liquids.listeners.EventListener;
-import ch.hsr.ifs.liquids.listeners.WindowListener;
+import ch.hsr.ifs.liquids.interfaces.Drawable;
+import ch.hsr.ifs.liquids.util.list.List;
 
 import com.sun.opengl.util.Animator;
 
-public class Liquids {
+public class Liquids implements GLEventListener {
 
-	private static final String FRAME_TITLE = "Liquids";
-
-	private Frame frame;
-	private Animator animator;
 	private GLCanvas canvas;
+	private Animator animator;
+	private Frame frame;
+
+	private List<Drawable> drawables = new List<Drawable>();
 
 	public Liquids() {
-		canvas = initialzeCanvas();
-		animator = initializeAnimator();
-		frame = initializeFrame();
+		initCanvas();
+		initAnimator();
+		initFrame();
 
-		frame.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
-		
 		animator.start();
 	}
 
-	private GLCanvas initialzeCanvas() {
-		GLCanvas canvas = new GLCanvas(new GLCapabilities());
-
-		canvas.addGLEventListener(new EventListener());
-
-		return canvas;
+	private void initCanvas() {
+		canvas = new GLCanvas(new GLCapabilities());
+		canvas.addGLEventListener(this);
 	}
 
-	private Animator initializeAnimator() {
-		return new Animator(canvas);
+	private void initAnimator() {
+		animator = new Animator(canvas);
 	}
 
-	private Frame initializeFrame() {
-		Frame frame = new Frame(FRAME_TITLE);
+	private void initFrame() {
+		frame = new Frame();
 
 		frame.add(canvas);
-		frame.addWindowListener(new WindowListener(animator));
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				animator.stop();
+				System.exit(0);
+			}
+		});
 
-		frame.setSize(800, 600);
-		frame.setLocationRelativeTo(null);
+		frame.setUndecorated(true);
+		frame.setAlwaysOnTop(true);
+		GraphicsDevice device = frame.getGraphicsConfiguration().getDevice();
+		device.setFullScreenWindow(frame);
+
 		frame.setVisible(true);
+	}
 
-		return frame;
+	public void init(GLAutoDrawable drawable) {
+		GL gl = drawable.getGL();
+
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+		gl.glMatrixMode(GL.GL_PROJECTION);
+		gl.glLoadIdentity();
+		gl.glOrtho(0.0d, 1.0d, 0.0d, 1.0d, -1.0d, 1.0d);
+	}
+
+	public void display(GLAutoDrawable autoDrawable) {
+		GL gl = autoDrawable.getGL();
+
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+		for (Drawable drawable : drawables) {
+			drawable.draw(gl);
+		}
+		gl.glFlush();
+	}
+
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width,
+			int height) {
+		System.out.println("reshape this, biatch!");
+		// TODO: implement method reshape
+	}
+
+	public void displayChanged(GLAutoDrawable drawable, boolean modeChanged,
+			boolean deviceChanged) {
+		System.out.println("who touched my display?!");
+		// TODO: implement method displayChanged
 	}
 
 	public static void main(String[] args) {
