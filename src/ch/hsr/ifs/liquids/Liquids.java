2844e759ec1018fa0b152ba8e4a72b4f93d8e794
mr_gimo <g.paulaitis@gmail.com>
2009-07-31 15:14:08 +0200

    basic implementation

--- src/ch/hsr/ifs/liquids/Liquids.java
+++ src/ch/hsr/ifs/liquids/Liquids.java
@@ -5,102 +5,100 @@ import java.awt.GraphicsDevice;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.media.opengl.GL;
-import javax.media.opengl.GLAutoDrawable;
-import javax.media.opengl.GLCanvas;
-import javax.media.opengl.GLCapabilities;
-import javax.media.opengl.GLEventListener;
+import javax.swing.JFrame;
 
-import ch.hsr.ifs.liquids.interfaces.Drawable;
-import ch.hsr.ifs.liquids.util.list.List;
+import ch.hsr.ifs.liquids.controller.Engine;
+import ch.hsr.ifs.liquids.screens.Screen;
+import ch.hsr.ifs.liquids.screens.StartUpScreen;
 
-import com.sun.opengl.util.Animator;
+public class Liquids {
 
-public class Liquids implements GLEventListener {
+	private Engine engine;
 
-	private GLCanvas canvas;
-	private Animator animator;
 	private Frame frame;
-
-	private List<Drawable> drawables = new List<Drawable>();
+	private Screen screen;
 
 	public Liquids() {
-		initCanvas();
-		initAnimator();
-		initFrame();
-
-		animator.start();
+		initialzeEngine();
+		initializeFrame();
 	}
 
-	private void initCanvas() {
-		canvas = new GLCanvas(new GLCapabilities());
-		canvas.addGLEventListener(this);
+	private void initialzeEngine() {
+		engine = new Engine();
 	}
 
-	private void initAnimator() {
-		animator = new Animator(canvas);
-	}
+	private void initializeFrame() {
+		frame = new JFrame();
 
-	private void initFrame() {
-		frame = new Frame();
+		configureFrame();
 
-		frame.add(canvas);
-		frame.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				animator.stop();
-				System.exit(0);
-			}
-		});
-
-		frame.setUndecorated(true);
-		frame.setAlwaysOnTop(true);
-		GraphicsDevice device = frame.getGraphicsConfiguration().getDevice();
-		device.setFullScreenWindow(frame);
+		addScreen(new StartUpScreen());
+		addWindowListener();
 
 		frame.setVisible(true);
 	}
 
-	public void init(GLAutoDrawable drawable) {
-		GL gl = drawable.getGL();
+	private void configureFrame() {
+		frame.setUndecorated(true);
+		frame.setAlwaysOnTop(true);
 
-		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-		gl.glMatrixMode(GL.GL_PROJECTION);
-		gl.glLoadIdentity();
-		gl.glOrtho(0.0d, 1.0d, 0.0d, 1.0d, -1.0d, 1.0d);
+		setFullscreen();
 	}
 
-	public void display(GLAutoDrawable autoDrawable) {
-		GL gl = autoDrawable.getGL();
+	private void setFullscreen() {
+		GraphicsDevice device = frame.getGraphicsConfiguration().getDevice();
+		device.setFullScreenWindow(frame);
+	}
 
-		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+	private void addWindowListener() {
+		frame.addWindowListener(new WindowAdapter() {
 
-		for (Drawable drawable : drawables) {
-			drawable.draw(gl);
+			@Override
+			public void windowClosing(WindowEvent e) {
+				frame.setVisible(false);
+				removeScreen();
+				System.exit(0);
+			}
+
+		});
+	}
+
+	private void addScreen(Screen newScreen) {
+		frame.add(newScreen);
+		engine.setScreen(newScreen);
+		newScreen.open();
+		screen = newScreen;
+	}
+
+	private void removeScreen() {
+		if (screen == null) {
+			return;
 		}
 
-		gl.glFlush();
+		screen.close();
+		frame.remove(screen);
+		screen = null;
 	}
 
-	public void reshape(GLAutoDrawable drawable, int x, int y, int width,
-			int height) {
-		System.out.println("reshape this, biatch!");
-		// TODO: implement method reshape
+	public void changeScreen(Screen newScreen) {
+		frame.setVisible(false);
+
+		removeScreen();
+		addScreen(newScreen);
+
+		frame.setVisible(true);
 	}
 
-	public void displayChanged(GLAutoDrawable drawable, boolean modeChanged,
-			boolean deviceChanged) {
-		System.out.println("who touched my display?!");
-		// TODO: implement method displayChanged
+	public Screen getScreen() {
+		return screen;
+	}
+
+	public Frame getFrame() {
+		return frame;
 	}
 
 	public static void main(String[] args) {
-		Liquids liquids = new Liquids();
-
-		for (int i = 0; i < 10000; ++i) {
-			liquids.drawables.add(new Particle(null));
-		}
+		new Liquids();
 	}
 
 }
